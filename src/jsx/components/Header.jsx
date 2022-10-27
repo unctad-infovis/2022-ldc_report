e34701a760ea913bce1b96454e757a5fe8cf7e80
Teemo Tebest <teemo.tebest@gmail.com>
2022-10-27 14:22:49 +0200

    Updated header

--- src/jsx/components/Header.jsx
+++ src/jsx/components/Header.jsx
@@ -4,7 +4,17 @@ function Header() {
   return (
     <>
       <div className="header_container">
-        <h2>LDC Report 2022</h2>
+        <h2>
+          Least Developed Countries
+          <br />
+          Report 2022
+        </h2>
+        <h3>46 countries</h3>
+        <h4>33 in Africa, 9 in Asia, 3 in the Pacific and 1 in The Caribbean</h4>
+        <h3>1.1 billion people</h3>
+        <h4>14% of the world’s population</h4>
+        <h3>Receive 69% of climate related deaths</h3>
+        <h4>But only account for 4% of green house emissions</h4>
         <svg className="arrows">
           <path className="a1" d="M0 0 L30 32 L60 0" />
           <path className="a2" d="M0 20 L30 52 L60 20" />
